afcc401a5406b4c3db3d2f7ec62762495ff9967e
songhee <thdgml5537@gmail.com>
2020-10-26 23:43:15 -0400

    add image and change 'home' and 'about' section style

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,16 @@ $(document).ready(function(){
         });
     });
     var w = document.documentElement.clientWidth;
+    var cardNum = $('.cardHeader');
+    var cardBody = $('.card-body');
+    var myPic = $('#myImage');
     if(w>992){
-        var cardNum = $('.cardHeader');
-        var cardBody = $('.card-body');
         cardNum.removeAttr('data-toggle');
         cardBody.removeClass('collapse');
+        myPic.attr('src', "./assets/images/songhee2.jpg")
+    }else{
+        cardNum.attr('data-toggle');
+        cardBody.addClass('collapse');
     }
 })
 
